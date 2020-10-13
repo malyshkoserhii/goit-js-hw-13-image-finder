782a3f6990b61b365a3eca03bf5ca753c5fc3e87
Malyshko Serhii <malyshkosergiy@gmail.com>
2020-10-13 21:17:36 +0300

    axios has been used in the homework

--- src/js/pixabay-service.js
+++ src/js/pixabay-service.js
@@ -1,21 +1,33 @@
+import axios from 'axios';
+
 const key = '18518367-60788b25c9bdd8e2c754a390a';
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
 
 export default {
   searchQuery: '',
   page: 1,
   async fetchPhotoes() {
-    try {
-      const url = `https://pixabay.com/api/?key=${key}&q=${this.searchQuery}&page=${this.page}&per_page=12`;
-      const request = await fetch(url);
-      const result = await request.json();
-      const data = await result.hits;
-      this.incrementPage();
+    const response = await axios.get(
+      `?key=${key}&q=${this.searchQuery}&page=${this.page}&per_page=12`,
+    );
 
-      return data;
-      
-    } catch (error) {
-      throw error;
-    }
+    return response.data.hits;
+
+    /* try-catch is the alternative way
+    // try {
+    //   const url = `https://pixabay.com/api/?key=${key}&q=${this.searchQuery}&page=${this.page}&per_page=12`;
+    //   const request = await fetch(url);
+    //   const result = await request.json();
+    //   const data = await result.hits;
+    //   this.incrementPage();
+
+    //   return data;
+
+    // } catch (error) {
+    //   throw error;
+    // }
+    */
   },
   resetPage() {
     this.page = 1;
